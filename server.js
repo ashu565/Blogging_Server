5a1d8969c2d0a5dc87a5afea548324645640eb9e
ashu565 <ashu9993063472@gmail.com>
2021-07-28 12:03:23 +0530

    fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ dotenv.config({
 });
 
 const app = require("./app");
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 const DB = process.env.DATABASE_URL.replace(
   "<PASSWORD>",
